2e0daeb4f6be6ad06cefc1682e49b979472242ed
XBananaMC <info@tech-weblog.net>
2014-10-29 18:08:37 +0100

    added update checker placeholder

---
+++ bootscript-beta.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#start cec listener
+# https://github.com/Noahgie/raspbmc-hdmi-cec-xbmc-shutdown-script for more info
+sh /usr/local/bin/cec_listener.sh
+
+#load updates
+bananian-update
+
+# install XBMC on first boot
+cd /home/xbmc
+sh first-boot.sh
+rm first-boot.sh
+
+.....................................
+# look for XBMC updates
+if
+  update then sh /home/xbmc/update.sh
+else
+  no-update
+fi
+.....................................
+
+
+#start hardware acceleration
+modprobe mali
